9e791cf1f494c63fdaa0e9d470e203e45cc1e07c
mddanish867 <mddanish867@gmail.com>
2025-01-08 19:55:28 +0530

    created new project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl font-extrabold bg-gradient-to-r from-purple-400 to-yellow-300 bg-clip-text text-transparent'>This is new application</h1>
+     
+    </>
+  )
+}
+
+export default App
